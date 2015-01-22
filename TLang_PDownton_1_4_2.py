fec430dfc5e2e3d23bfc7d6bf433dd9196666459
Trevan X Lang <langtrevan@gmail.com>
2015-01-22 08:12:18 -0600

    tran progress

--- TLang_PDownton_1_4_2.py
+++ TLang_PDownton_1_4_2.py
@@ -2,10 +2,12 @@
 '''
 Tlang_PDownton_1_4_2: Read and show an image.
 '''
+import PIL.ImageDraw
+import PIL
 import matplotlib.pyplot as plt 
 import os.path
 import numpy as np      # “as” lets us use standard abbreviations
-
+import matplotlib.colors as pltc
 '''Read the image data'''
 # Get the directory of this python script
 directory = os.path.dirname(os.path.abspath(__file__)) 
@@ -18,7 +20,9 @@ width = len(img[0])
 for r in range(height):
     for c in range(width):
         if sum(img[r][c])==765: # brightness R+G+B goes up to 3*255=765
-            img[r][c][img[r][c]]=[255]
+            img[r][c]=[pltc.ColorConverter.to_rgba(pltc.ColorConverter(img[r][c]), alpha=1)[0],
+            pltc.ColorConverter.to_rgba(pltc.ColorConverter(img[r][c]), alpha=1)[1],
+            pltc.ColorConverter.to_rgba(pltc.ColorConverter(img[r][c]), alpha=1)[2]]
 # Create figure with 2 subplots
 fig, ax = plt.subplots(1, 1)
 # Show the image data in the first subplot
